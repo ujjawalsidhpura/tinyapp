2c162bc14f9cae0ddac8f052a90ff1bab7ff79d6
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-27 21:06:00 +0000

    step 1

--- express_server.js
+++ express_server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+
+//Use body parser for FORM POST requests
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+//Temp Database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//function 
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6);
+}
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -19,6 +27,13 @@ app.get('/urls', (req, res) => {
 //GET route to Present FORM to the user(browser).GETTING the form from server to the user. Server will respond with urls_new and using ejs, it will render an HTML form.
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
+})
+
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  req.body.shortURL = shortURL;
+  console.log(req.body);
+  res.send('OK');
 })
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -32,3 +47,5 @@ app.get('/urls/:shortURL', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening to ${PORT}`);
 });
+
+
